27002fef1df6f6d15b8522991c95004c4d992b60
Michael F. Herbst <info@michael-herbst.com>
2020-03-11 21:36:34 +0100

    Fixup computation of chi0

--- src/postprocess/chi_0.jl
+++ src/postprocess/chi_0.jl
@@ -1,4 +1,7 @@
 using ForwardDiff
+using ProgressMeter
+
+
 """
 Compute the independent-particle susceptibility. Will blow up for large systems
 """
@@ -9,30 +12,27 @@ function compute_χ0(ham)
     @assert length(basis.kpoints) == 1
     @assert model.spin_polarisation == :none
     filled_occ = DFTK.filled_occupation(model)
-    N = length(basis.kpoints[1].basis)
+    N = length(G_vectors(basis.kpoints[1]))
     @assert N < 10_000
-    println("Computing χ0 with N=$N")
 
     Hk = ham.blocks[1]
     E, V = eigen(Hermitian(Array(Hk)))
     occ, εF = DFTK.find_occupation(basis, [E])
     occ = occ[1]
-    Vr = G_to_r(basis, basis.kpoints[1], V)
+    Vr = hcat(G_to_r.(Ref(basis), Ref(basis.kpoints[1]), eachcol(V))...)
     Vr = reshape(Vr, prod(fft_size), N)
     χ0 = zeros(eltype(V), prod(fft_size), prod(fft_size))
-    for m = 1:N
-        println("$m/$N")
-        for n = 1:N
-            @assert occ[n] ≈ filled_occ * model.smearing((E[n] - εF) / model.temperature)
-            if occ[n] < 1e-6 && occ[m] < 1e-6
-                factor = 0.0
-            elseif abs(E[m] - E[n]) < 1e-6
-                factor = filled_occ * ForwardDiff.derivative(ε -> model.smearing((ε - εF) / model.temperature), E[n])
-            else
-                factor = (occ[m] - occ[n])/(E[m] - E[n])
-            end
-            χ0 += (Vr[:, m] .* Vr[:, m]') .* (Vr[:, n] .* Vr[:, n]') * factor
+    @showprogress "Computing χ0 ..." for m = 1:N, n = 1:N
+        enred = (E[n] - εF) / model.temperature
+        @assert occ[n] ≈ filled_occ * Smearing.occupation(model.smearing, enred)
+        if occ[n] < 1e-6 && occ[m] < 1e-6
+            factor = 0.0
+        elseif abs(E[m] - E[n]) < 1e-6
+            factor = filled_occ * Smearing.occupation_derivative(model.smearing, enred) / model.temperature
+        else
+            factor = (occ[m] - occ[n])/(E[m] - E[n])
         end
+        χ0 += (Vr[:, m] .* Vr[:, m]') .* (Vr[:, n] .* Vr[:, n]') * factor
     end
     χ0
 end
